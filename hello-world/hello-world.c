3c9911356df8764104d0e30f451bb8364a366e75
Marcos Túlio <marcostuliio99@gmail.com>
2020-12-29 16:57:38 -0300

    add funcions: verify version language c; asc

--- hello-world/hello-world.c
+++ hello-world/hello-world.c
@@ -1,5 +1,14 @@
 #include <stdio.h>    
-int main(){    
-printf("Kabum!!!");    
+int main(void){
+//printf("Hello World!!!");
+/*
+int stdc = __STDC_VERSION__;
+printf("%u",stdc);
+*/
+puts("Printable ASCII:");
+    for (int i = 32; i < 127; ++i) {
+        putchar(i);
+        putchar(i % 16 == 15 ? '\n' : ' ');
+    }
 return 0;   
 }
