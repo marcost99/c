6d4f72c7b4e7b3ebb1c98294a760a374b4e264ee
Marcos Túlio <marcostuliio99@gmail.com>
2020-12-31 18:23:39 -0300

    modify program hello-world for support debugger information

--- hello-world/hello-world.c
+++ hello-world/hello-world.c
@@ -1,8 +1,12 @@
 #include <stdio.h>    
 int main(void){
+int num = 10;
+printf("Number: %u",num);
 //printf("Hello World!!!");
+/*
 int stdc = __STDC_VERSION__;
 printf("%u",stdc);
+*/
 /*
 puts("Printable ASCII:");
     for (int i = 32; i < 127; ++i) {
