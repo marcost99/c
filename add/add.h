be414c60af320f64ea13652822306c6ddf79331f
Marcos Túlio <marcostuliio99@gmail.com>
2021-01-01 11:36:17 -0300

    starting project add

---
+++ add/add.h
@@ -0,0 +1,6 @@
+#ifndef ADD_H_INCLUDED
+#define ADD_H_INCLUDED
+
+int add(int quant1, int quant2);
+
+#endif
---
+++ add/addDemo.c
@@ -0,0 +1,9 @@
+#include <add.h>
+#include <stdio.h>
+ 
+int main(void)
+{
+  int x= 10, y = 20;
+  printf("\n%d + %d = %d", x, y, add(x, y));
+  return 0;
+}
